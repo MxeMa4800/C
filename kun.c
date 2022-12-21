5dd4b59f9f0d3eea58fd607aed0b21ea26265fbc
MxaMa4800 <awanta.aworn.4800@gmail.com>
2022-12-21 16:51:15 +0700

    first commit

--- kun.c
+++ kun.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int sum(int,int);
+int winlos(int a[]);
+
 int main()
 {
-    printf("hello");
+    char* row[5] = {"1","2","3","4","5","6"};
+    int win[5] = {0};
+    int sum1 = 0;
+    int sd = 0;
+    int num = 0;
+    int dice1 = 0;
+    int dice2 = 0;
 }
